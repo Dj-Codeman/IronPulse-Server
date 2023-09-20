185dd73bdd9ac33d7fa6996b24c4b241e9927dc3
Darrion Whitfield <dwhitfield@ramfield.net>
2023-09-19 20:07:23 -0500

    Handel database struct

--- src/database.rs
+++ src/database.rs
@@ -1,18 +1,21 @@
 use core::panic;
 
 use logging::append_log;
-use  system::del_file;
 use mysql::*;
+use pretty::halt;
 use recs::retrive;
 use std::fs;
+use system::del_file;
 
-use crate::{PROG, skel::Database};
+use crate::{skel::Database, PROG};
 
-#[allow(non_snake_case)]
 pub fn create_pool() -> Pool {
     let (db_username, db_password, db_host, database) = match read_credentials() {
         Some(db) => (db.username, db.password, db.hostaddr, db.database),
-        None => todo!(),
+        None => {
+            halt("No database credentials found, use rescs and store database credentials as Ironpulse, database, /tmp/database.dk");
+            panic!()
+        }
     };
 
     let url: String = format!(
@@ -38,25 +41,25 @@ fn read_credentials() -> Option<Database> {
                 // retrive the data
                 let database_creds: String = match fs::read_to_string("/tmp/database.dk") {
                     Ok(data) => data,
-                    Err(e) => { 
-                        append_log(PROG, &format!("Error reading database credentials: {}", e)); 
+                    Err(e) => {
+                        append_log(PROG, &format!("Error reading database credentials: {}", e));
                         panic!();
-                    },
+                    }
                 };
                 del_file("/tmp/database.dk");
-                // unpack and map 
+                // unpack and map
                 let data: Vec<String> = database_creds.split('/').map(|s| s.to_string()).collect();
-                Some(Database{
+                return Some(Database {
                     username: data[0].to_owned(),
                     password: data[1].to_owned(),
                     hostaddr: data[2].to_owned(),
                     database: data[3].to_owned(),
-                })
-            },
+                });
+            }
             false => {
                 append_log(PROG, "Could not read database credentials from recs");
                 panic!("Check logs");
-            },
+            }
         },
         None => panic!("Unable to communicate with recs"),
     }
